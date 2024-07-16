565660d960164fc7e11e555e1df20c61c89b39f9
JonKadriu <jon.f.kadriu@gmail.com>
2024-07-16 11:35:34 +0200

    Add files via upload

---
+++ ushtrim.py
@@ -0,0 +1,27 @@
+person = {
+    "Emri": "Jon",
+    "Mbiemri": "Kadriu",
+    "Kompania": "jCoders",
+    "Titulli": "Trainer",
+    "Mosha": 16
+}
+if "Kompania" in person:
+    print("Po eshte ne objekt")
+else:
+    print("Jo nuk eshte ne objekt")
+
+
+###Menyra 1
+#for x in person:
+#    print(person[x])
+
+###Menyra 2
+#for i in person:
+#print(i)
+
+#emri = person["Emri"]
+#print(emri)
+
+
+#person["Titulli"] = "student"
+#print(person)
